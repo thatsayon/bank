52cd2ac12792e596b64e076c7f9d35d19f510533
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2023-10-13 15:44:05 +0600

    First step completed

--- User.py
+++ User.py
@@ -1,18 +1,26 @@
 class User:
     accounts = []
 
-    def __init__(self, name, email, address, acc_type):
+    def __init__(self, name, email, address, password, acc_type):
         self.name = name
         self.email = email
         self.address = address
+        self.password = password
         self.acc_type = acc_type
         User.accounts.append(self)
 
 
 class Client(User):
-    def __init__(self, name, email, address, acc_type):
-        super().__init__(name, email, address, acc_type)
-        print("Created")
+    def __init__(self, name, email, address, password, acc_type):
+        super().__init__(name, email, address, password, acc_type)
+        print("\nAccount Created Successfully!!!\nPlease log in now to use our facilities\n")
+
+    @staticmethod
+    def log_in(email, password):
+        for users in User.accounts:
+            if email == users.email and password == users.password:
+                return True
+        return False
 
 
 class Admin(User):
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from User import Admin, Client
 
 def main():
     while True:
-        print("*"*20)
-        print("For User Access Type 1\nFor Admin Access Type 2")
-        print("*"*20)
-        user_input = int(input("Input 1 or 2: "))
+        print("*" * 20)
+        print("For User Access Type 1\nFor Admin Access Type 2\nTo close the application type 3")
+        print("*" * 20)
+        user_input = int(input("Input 1, 2 or 3: "))
 
         # code to manage a user
         if user_input == 1:
@@ -14,14 +14,25 @@ def main():
             print("To log in type 1\nTo create a new account type 2")
             print("*" * 20)
             user_input = int(input("Input 1 or 2:"))
+
+            # log in into the account
             if user_input == 1:
-                pass
+                email = input("Enter your email address: ")
+                password = input("Enter your password: ")
+                if Client.log_in(email, password):
+                    pass
+                else:
+                    print("Wrong email or password")
+
+
+            # create an account
             elif user_input == 2:
                 username = input("Enter your name: ")
                 email = input("Enter your email address: ")
                 address = input("Enter your address: ")
+                password = input("Enter your password: ")
                 acc_type = input("Account Type: Savings or Current (S/C): ")
-                Client(username, email, address, acc_type)
+                Client(username, email, address, password, acc_type)
 
         # code to manage an admin
         elif user_input == 2:
@@ -31,11 +42,20 @@ def main():
             print("*" * 20)
             if admin_name == 'admin' and admin_pass == '123':
                 print("Successfully Login")
-                print("-"*20)
-                print("3. See all user accounts list")
-                admin_in = int(input("What you want to do?: "))
-                if admin_in == 3:
-                    Admin.show_accounts()
+                print("-" * 20)
+                while True:
+                    print("3. See all user accounts list")
+                    print("7. Exit from admin panel")
+                    admin_in = int(input("What you want to do?: "))
+                    if admin_in == 3:
+                        Admin.show_accounts()
+                    elif admin_in == 7:
+                        break
+            else:
+                print("Wrong username or password")
+
+        elif user_input == 3:
+            break
 
 
 if __name__ == '__main__':
